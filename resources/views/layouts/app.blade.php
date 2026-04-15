2d41390a47a5abf73bd2cd16793581c29250528e
BART <your-github-email@example.com>
2026-04-15 22:31:39 +0800

    Remove JS bundle loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'Laravel'))</title>
-    @if (file_exists(public_path('build/manifest.json')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
     <style>
         :root {
             color-scheme: light;
