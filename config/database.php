00439df5de173f74dab09389a2079eb1b04cf0f4
BART <your-github-email@example.com>
2026-04-16 16:34:26 +0800

    Switch from MySQL to PostgreSQL for Railway

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', env('DATABASE_URL') ? 'mysql' : 'sqlite'),
+    'default' => env('DB_CONNECTION', env('DATABASE_URL') ? 'pgsql' : 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
